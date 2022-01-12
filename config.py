5cebab0bcccfa3826b7d4cbb1ae3b4bb7c476233
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-01-12 19:04:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,11 +10,10 @@ get_queue = {}
 STRING = getenv("STRING_SESSION", "AQChKll4pT0GVgeBvJup9bez3tsdsj7pgjIfdzS7oQBnS5fmFPze-sE8rBqohlwjGVBwDfsQfXXEfDAaipUOjemBKv6ZTgHd7b_-dQAYzuMJKHhX4plxEfQKxDAHiu7RAwoDh--vqmJCDfzDTH0HA9uuv3179I3o-P1E5-jh_Bo77zo3G7dUweSy52zUyJgAO8cp1leImoMx79XDiypT0H4ASH6ygvN3f2FCFkPi5tPo8ZBBK4B9z0UCdqyhZXfseCHNrgd8WBMJlPy7jdEbpFM0vujzF_ZCnO_LJowJmoJE59dBN4tN1oYY0zR1DT0YBISdLDUeTuEc7X-2LtplwJiJeSrSXAA")
 BOT_TOKEN = getenv("BOT_TOKEN" , "1607821276:AAGuamsqQ2pe9yHz1fn9kWuI268o_vO9Yuc")
 API_ID = int(getenv("API_ID", "15816197"))
-API_HASH = getenv("API_HASH", "42a79ffac6e037eb1a2671298c34d5de)
+API_HASH = getenv("API_HASH", "42a79ffac6e037eb1a2671298c34d5de")
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", "15"))
 ASSISTANT_PREFIX = list(getenv("ASSISTANT_PREFIX", ".").split())
-MONGO_DB_URI = getenv("MONGO_DB_URI" , "mongodb+srv://Aman:Aman@cluster0.kigqe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
-")
+MONGO_DB_URI = getenv("MONGO_DB_URI" , "mongodb+srv://Aman:Aman@cluster0.kigqe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1668305941").split()))
 OWNER_ID = list(map(int, getenv("OWNER_ID", "1668305941").split()))
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001654637381)
