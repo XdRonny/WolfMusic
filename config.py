8236e113e8e5a6707f39760ef8a3a27278c8b930
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-01-12 18:57:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,23 +7,24 @@ load_dotenv()
 # VARS
 
 get_queue = {}
-STRING = getenv("STRING_SESSION", "AQAc48PgIQA6JzRUfDTLADNi4n_Hg-EBFaurHr8WhYMaOEj7GNk82hFKJ0aPfKvvOVkZ45v9Xx9fRJm8SkuhK6XAxlDl6aTIBSuvUusyjGKFL8wz9kYNsOC6S2q5i5rcb6k77ztrL5vefAxf5bBRvPfkMhMeOzHrq-PbSsavXBOjuOa_VYS6XBk0N5aGiWDryJoqk1yhbaX3g7thol1Yjmk7jLstqjBwN6JkqwP5W82a6fRiKXgqkQzEyarnXYhlby7vH7QbLCy88yVLuNZFflVgSGYFAKInYJMTEwupfZUFpsnGGAnZn2VC_6FidfZ5NPJwL6NVDFtLs17REEbrrWsRAAAAAS4fyOgA")
-BOT_TOKEN = getenv("BOT_TOKEN" , "2100234484:AAEb0S-CWC4h2Xr1rOUft9PvbqPKszSnCxQ")
-API_ID = int(getenv("API_ID", "14486017"))
-API_HASH = getenv("API_HASH", "e303f55d6a548e29ca7e91cadbd80182")
-DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", "10"))
+STRING = getenv("STRING_SESSION", "AQChKll4pT0GVgeBvJup9bez3tsdsj7pgjIfdzS7oQBnS5fmFPze-sE8rBqohlwjGVBwDfsQfXXEfDAaipUOjemBKv6ZTgHd7b_-dQAYzuMJKHhX4plxEfQKxDAHiu7RAwoDh--vqmJCDfzDTH0HA9uuv3179I3o-P1E5-jh_Bo77zo3G7dUweSy52zUyJgAO8cp1leImoMx79XDiypT0H4ASH6ygvN3f2FCFkPi5tPo8ZBBK4B9z0UCdqyhZXfseCHNrgd8WBMJlPy7jdEbpFM0vujzF_ZCnO_LJowJmoJE59dBN4tN1oYY0zR1DT0YBISdLDUeTuEc7X-2LtplwJiJeSrSXAA")
+BOT_TOKEN = getenv("BOT_TOKEN" , "1607821276:AAGuamsqQ2pe9yHz1fn9kWuI268o_vO9Yuc")
+API_ID = int(getenv("API_ID", "15816197"))
+API_HASH = getenv("API_HASH", "42a79ffac6e037eb1a2671298c34d5de)
+DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", "15"))
 ASSISTANT_PREFIX = list(getenv("ASSISTANT_PREFIX", ".").split())
-MONGO_DB_URI = getenv("MONGO_DB_URI" , "mongodb+srv://xd:xd@cluster0.wb93b.mongodb.net/xd?retryWrites=true&w=majority")
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1920507972").split()))
-OWNER_ID = list(map(int, getenv("OWNER_ID", "5032100535").split()))
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001781944400"))
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "Florenza_Musix")
+MONGO_DB_URI = getenv("MONGO_DB_URI" , "mongodb+srv://Aman:Aman@cluster0.kigqe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1668305941").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "1668305941").split()))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001654637381)
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "Innexia_X_Music")
 PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/732363bb2cafd8281aadd.jpg")
-if str(getenv("SUPPORT_CHANNEL", "STARZ_BOTS")).strip() == "":
+if str(getenv("SUPPORT_CHANNEL", "RaichuUpdate")).strip() == "":
     SUPPORT_CHANNEL = None
 else:
-    SUPPORT_CHANNEL = str(getenv("SUPPORT_CHANNEL", "STARZ_BOTS"))
-if str(getenv("SUPPORT_GROUP", "Starz_Support")).strip() == "":
+    SUPPORT_CHANNEL = str(getenv("SUPPORT_CHANNEL", "RaichuUpdate")
+if str(getenv("SUPPORT_GROUP", "RaichuUpdate")).strip() == "":
     SUPPORT_GROUP = None
 else:
-    SUPPORT_GROUP = str(getenv("SUPPORT_GROUP", "Starz_Support"))
+    SUPPORT_GROUP = str(getenv("SUPPORT_GROUP", "RaichuOfficial"))
