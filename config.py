75342ff54327e62a69732d7b3617bea9ad93afad
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-01-12 20:28:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ load_dotenv()
 # VARS
 
 get_queue = {}
-STRING = getenv("STRING_SESSION", "")
+STRING = getenv("STRING_SESSION", "AQCdTDw-S3vqOPG6xe4AloXwWln_VelvwnoWO3zm_2YHFsLWKPckDmjhyH2wQoOqNEr5re_Q1pdBqNMzcLHNcaoKEqUPoXB5_lllCpa7tu1dnjBfpe6TSM116rRfm44P18pbw7r16tJ9wSOrxiyRmnUShy2ku_ubSKbcgjTLs2oDAFcKeh6rEtJMMcl7PUzI-VSrsQRQUKIElP-Sb1IF3-sVwf-320kRbLF7X6T2uGIr0bvpFZUnfUmLc5ZQndS-SKELqa_LBoJuOGUXrzKcj5KDevlYaLdgZoXvyAycNjnPRn4nRnJRyyiUwF0shojBBZwjk5NG-U2_3zm5mds9-EDleSrSXAA")
 BOT_TOKEN = getenv("BOT_TOKEN" , "1607821276:AAGuamsqQ2pe9yHz1fn9kWuI268o_vO9Yuc")
 API_ID = int(getenv("API_ID", "15816197"))
 API_HASH = getenv("API_HASH", "42a79ffac6e037eb1a2671298c34d5de")
